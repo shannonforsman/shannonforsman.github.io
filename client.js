503637bd876ad1bfd464e743d5ced3adc361442e
Shannon Forsman <sforsman1@gmail.com>
2015-08-18 14:31:45 -0600

    added files from project repo

---
+++ client.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+  var items = $('.item')
+
+$('.item:first-child .overlay').css({'opacity': '0'})
+
+items.on('click', function() {
+  $('.show').removeClass('portfolio')
+  $('.overlay').css({'background-color': '', 'opacity' : ''})
+  $(this).find('.overlay').css({'background-color': 'none', 'opacity': '0'})
+  var open = $(this).data('grid');
+  console.log(open)
+  $('#' + open).addClass('portfolio');
+})
+
+})
