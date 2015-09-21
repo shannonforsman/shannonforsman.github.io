a9328e13fb364011e17e72096aecf953a0fafcfd
Shannon Forsman <sforsman1@gmail.com>
2015-09-20 22:27:19 -0600

    fixed small formatting issues

--- client.js
+++ client.js
@@ -1,16 +1,14 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
   var items = $('.item')
 
-$('.item:first-child .overlay').css({'opacity': '0'})
+  $('.item:first-child .overlay').css({'opacity': '0'})
 
-items.on('click', function() {
-  $('.show').removeClass('portfolio')
-  $('.overlay').css({'background-color': '', 'opacity' : ''})
-  $(this).find('.overlay').css({'background-color': 'none', 'opacity': '0'})
-  var open = $(this).data('grid');
-  console.log(open)
-  $('#' + open).addClass('portfolio');
-})
-
+  items.on('click', function () {
+    $('.show').removeClass('portfolio')
+    $('.overlay').css({'background-color': '', 'opacity': ''})
+    $(this).find('.overlay').css({'background-color': 'none', 'opacity': '0'})
+    var open = $(this).data('grid')
+    $('#' + open).addClass('portfolio')
+  })
 })
