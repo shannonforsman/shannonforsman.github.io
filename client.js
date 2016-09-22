a3b7895ea3a167577af40dd5c4ecc8c0ba0733f6
Shannon Forsman <sforsman1@gmail.com>
2016-09-21 22:10:47 -0600

    added subreddit section

--- client.js
+++ client.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
 
-  var items = $('.item')
+  var items = $('.item');
 
-  $('.item:first-child .overlay').css({'opacity': '0'})
+  $('.item:first-child .overlay').css({'opacity': '0'});
 
   items.on('click', function () {
-    $('.show').removeClass('portfolio')
-    $('.overlay').css({'background-color': '', 'opacity': ''})
-    $(this).find('.overlay').css({'background-color': 'none', 'opacity': '0'})
-    var open = $(this).data('grid')
-    $('#' + open).addClass('portfolio')
+    $('.show').removeClass('portfolio');
+    $('.overlay').css({'background-color': '', 'opacity': ''});
+    $(this).find('.overlay').css({'background-color': 'none', 'opacity': '0'});
+    var open = $(this).data('grid');
+    $('#' + open).addClass('portfolio');
   })
-})
+});
